50ede164219a6001e81c52e99f5a72f24325595b
Klaudiusz Kolegowicz <klaudiusz.kolegowicz@microsoft.wsei.edu.pl>
2022-07-01 21:26:13 +0200

    Dodaj pliki projektów.

---
+++ lab9/Comboboxbind.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_9
+{
+    public partial class MainWindow
+    {
+        public class Comboboxbind {
+            public int wielkosc { get; set; }
+            public int wielkosc_wyswietlana { get; set; }
+                }
+    }
+}
